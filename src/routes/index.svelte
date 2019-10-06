9983759fe2a28934eb5fade21848426f7d6a7fd6
Sameer Tariq <imsamtar@gmail.com>
2019-10-06 12:00:49 +0500

    Refactored How information is displaedl

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,11 +23,22 @@
 	button:hover {
 		background: #1b1b1b;
 	}
-	#img {
-		border-radius: 50%;
-	}
 	#name {
 		text-transform: capitalize;
+	}
+	main {
+		display: flex;
+		flex-flow: wrap;
+	}
+	main > * {
+		padding: 0.5rem;
+	}
+	.fc {
+		display: flex;
+		flex-flow: column;
+	}
+	.fc > span > b {
+		margin-right: 1rem;
 	}
 </style>
 
@@ -35,20 +46,28 @@
 	<title>Fake Person Generator</title>
 </svelte:head>
 <button on:click={fetchPerson}>New</button>
-{#if p}
-	<img id="img" loading="lazy" src="{p.picture.large}" alt="{p.name.title} {p.name.first} {p.name.last}">
-	<h4 id="name"> <b>Name:</b> {p.name.first} {p.name.last}</h4>
-	<h4 id="gender"> <b>Gender:</b> {p.gender}</h4>
-	<h4 id="date"> <b>Date of Birth:</b> {p.dob.date}</h4>
-	<h4 id="age:"> <b>Age:</b> {p.dob.age}</h4>
-	<h4 id="emai"> <b>Email:</b> {p.email} {p.phone}</h4>
-	<h4 id="nat"> <b>Nationality:</b> {p.nat}</h4>
-	<h4 id="cell"> <b>Cell:</b> {p.cell}</h4>
-	<h4 id="login"> <b>Login:</b> {p.id.name}</h4>
-	<h4 id="user"> <b>Username:</b> {p.login.username}</h4>
-	<h4 id="pass"> <b>Password:</b> {p.login.password}</h4>
-	<h4 id="loca"> <b>Location:</b> {p.location.street}, {p.location.city}, {p.location.state}</h4>
-	<h4 id="postcode"> <b>Postcode:</b> {p.location.postcode}</h4>
-	<h4 id="regage"> <b>Registered Age:</b> {p.registered.age}</h4>
-	<h4 id="regdate"> <b>Registration Date:</b> {p.registered.date}</h4>
-{/if}
+<main>
+	{#if p}
+		<div>
+			<img id="img" loading="lazy" src="{p.picture.large}" alt="{p.name.title} {p.name.first} {p.name.last}">
+		</div>
+		<div class="fc">
+			<span id="name"><b>Name:</b> {p.name.first} {p.name.last} ({p.gender})</span>
+			<span id="username"><b>Username:</b> @{p.login.username}</span>
+			<span id="email"><b>Email:</b> {p.email}</span>
+			<span id="phone"><b>Phone:</b> {p.phone}</span>
+			<span id="date"><b>DOB:</b> {p.dob.date.slice(0, 10)} (age = {p.dob.age})</span>
+			<!-- {@debug p} -->
+		</div>
+		<div class="fc">
+			<span id="nat"> <b>Nationality:</b> {p.nat}</span>
+			<span id="cell"> <b>Cell:</b> {p.cell}</span>
+			<span id="login"> <b>Login:</b> {p.id.name}</span>
+			<span id="pass"> <b>Password:</b> {p.login.password}</span>
+			<span id="loca"> <b>Location:</b> {p.location.street.number+' '+p.location.street.name}, {p.location.city}, {p.location.state}</span>
+			<span id="postcode"> <b>Postcode:</b> {p.location.postcode}</span>
+			<span id="regage"> <b>Registered Age:</b> {p.registered.age}</span>
+			<span id="regdate"> <b>Registration Date:</b> {p.registered.date}</span>
+		</div>
+	{/if}
+</main>
